ea4f88c7da0a092b2dbef07d23ae7e5bc62737c0
stepani212 <147595332+stepani212@users.noreply.github.com>
2024-02-05 13:44:55 +0700

    Add Model Investasi

---
+++ app/Http/Controllers/InvestasiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Investasi;
+use App\Http\Requests\StoreInvestasiRequest;
+use App\Http\Requests\UpdateInvestasiRequest;
+
+class InvestasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInvestasiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Investasi $investasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Investasi $investasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInvestasiRequest $request, Investasi $investasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Investasi $investasi)
+    {
+        //
+    }
+}
