f789662456d0617cdf37f2a52995242cc984d202
stepani212 <147595165+Adinsyafira10@users.noreply.github.com>
2024-02-05 14:13:57 +0700

    Update v_sidebar.blade.php

--- resources/views/layouts/v_sidebar.blade.php
+++ resources/views/layouts/v_sidebar.blade.php
@@ -24,7 +24,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+            <img src="{{ asset('img') }}/logo.png" alt="" style="width: 30px;">
         </div>
         <div class="sidebar-brand-text mx-3">Manajemen Investasi</div>
     </a>
