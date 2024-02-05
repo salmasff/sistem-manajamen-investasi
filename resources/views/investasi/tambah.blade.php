8b3a9569d0eac36d923fb692328a9f1f98cb3638
stepani212 <147794510+Tiaindriani03@users.noreply.github.com>
2024-02-05 14:01:59 +0700

    Update tambah.blade.php

--- resources/views/investasi/tambah.blade.php
+++ resources/views/investasi/tambah.blade.php
@@ -33,7 +33,24 @@
                         <div class="card-body">
                             <form action="/investasi/store" method="post">
                                 {{ csrf_field() }}
-                                
+                                <div class="card-body">
+                                    <div class="form-group">
+                                      <label for="exampleInputEmail1">Nama Perusahaan</label>
+                                      <input type="text" class="form-control" id="exampleInputEmail1" name="nama_perusahaan" placeholder="Masukan Nama Perusahaan" required>
+                                    </div>
+                                    <div class="form-group">
+                                      <label for="exampleInputEmail1">Nama Investor</label>
+                                      <input type="text" class="form-control" id="exampleInputEmail1" name="nama_investor" placeholder="Masukan Nama Investor" required>
+                                    </div>
+                                    <div class="form-group">
+                                      <label for="jml">Jumlah Investasi</label>
+                                      <input type="number" class="form-control" id="jml" name="jml_investasi" placeholder="Masukan Jumlah Investasi" required>
+                                    </div>
+                                    <div class="form-group">
+                                      <label for="exampleInputPassword1">Tanggal Investasi</label>
+                                      <input type="date" class="form-control" id="exampleInputPassword1" name="tgl_investasi" placeholder="Masukan Tanggal Investasi" required>
+                                    </div>
+                                  </div>
                                 <!-- /.card-body -->
                 
                                 <div class="card-footer">
