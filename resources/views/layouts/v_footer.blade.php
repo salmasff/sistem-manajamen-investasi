eb04e80276d7846ac6b21fd543676dc00a17573e
AsmaNabilah <147595165+Adinsyafira10@users.noreply.github.com>
2024-02-05 13:41:08 +0700

    Update v_footer.blade.php

---
+++ resources/views/layouts/v_footer.blade.php
@@ -0,0 +1,30 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Kelompok 4 2024</span>
+        </div>
+    </div>
+</footer>
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset('template') }}/vendor/jquery/jquery.min.js"></script>
+<script src="{{ asset('template') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{ asset('template') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('template') }}/js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="{{ asset('template') }}/vendor/chart.js/Chart.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="{{ asset('template') }}/js/demo/chart-area-demo.js"></script>
+<script src="{{ asset('template') }}/js/demo/chart-pie-demo.js"></script>
