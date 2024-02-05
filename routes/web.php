e7af5fbb7a440ddd460887af6b06f29ea73cc4e9
stepani212 <147595267+salmasff@users.noreply.github.com>
2024-02-05 14:07:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::get('/investasi', 'App\Http\Controllers\InvestasiController@index')->middleware(['auth', 'verified'])->name('home');
 Route::get('/investasi/tambah','App\Http\Controllers\InvestasiController@create')->middleware(['auth', 'verified'])->name('tambah');
 Route::post('/investasi/store','App\Http\Controllers\InvestasiController@store')->middleware(['auth', 'verified'])->name('tambah2');
